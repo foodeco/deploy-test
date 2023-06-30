4395f5d5cc38e3caea61a074e74d923342ed25e9
Seo Donguk <tjehddnr997@gmail.com>
2023-06-30 17:43:27 +0900

    style: static file 경로 조정

--- src/Common/Layout/Header/header.tsx
+++ src/Common/Layout/Header/header.tsx
@@ -1,9 +1,9 @@
-import { Link, useNavigate  } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { ChangeEvent } from "react";
-import "./headers.scss";
-import { LogoutForm,TokenMe } from "@/Apis/register";
-import { getList } from "@/Apis/productApi";
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ChangeEvent } from 'react';
+import './headers.scss';
+import { LogoutForm, TokenMe } from '@/Apis/register';
+import { getList } from '@/Apis/productApi';
 
 interface User {
   displayName: string; // 사용자 표시 이름
@@ -18,22 +18,22 @@ interface Product {
 }
 
 function Header() {
-  const defaultProfileImgUrl = "/public/images/default-profile.jpg";
-  const [user, setUser] = useState<User>({ displayName: "", profileImg: "" });
-  const [keyword, setKeyWord] = useState("");
+  const defaultProfileImgUrl = '/public/images/default-profile.jpg';
+  const [user, setUser] = useState<User>({ displayName: '', profileImg: '' });
+  const [keyword, setKeyWord] = useState('');
   const [filteredItems, setFilteredItems] = useState([]);
   const [product, setProductInfo] = useState([]);
   const [showInputButton, setShowInputButton] = useState(false);
   const navigate = useNavigate();
-  
+
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setKeyWord(e.target.value);
-    setShowInputButton(e.target.value.trim() !== "");
+    setShowInputButton(e.target.value.trim() !== '');
   };
 
   const handleInputButtonClick = () => {
-    if (keyword === "") {
-      alert("검색어를 입력해주세요");
+    if (keyword === '') {
+      alert('검색어를 입력해주세요');
     } else {
       onSubmit();
     }
@@ -42,26 +42,26 @@ function Header() {
   const logoutHandler = () => {
     LogoutForm()
       .then(() => {
-        localStorage.removeItem("token");
-        alert("로그아웃 되셨습니다");
-        navigate("/");
+        localStorage.removeItem('token');
+        alert('로그아웃 되셨습니다');
+        navigate('/');
       })
       .catch((error: string) => {
-        console.log("Logout failed:", error);
+        console.log('Logout failed:', error);
       });
   };
 
   const onSubmit = async () => {
-    navigate("/search/" + keyword);
+    navigate('/search/' + keyword);
   };
 
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
     const authenticate = async () => {
       try {
-        const userData = await TokenMe(); 
-  
+        const userData = await TokenMe();
+
         // 사용자 정보를 업데이트하기 전에 profileImg가 존재하지 않을 경우에만 기본 프로필 이미지 URL을 사용
         setUser((prevUser) => ({
           ...prevUser,
@@ -76,11 +76,11 @@ function Header() {
       authenticate();
     }
   }, [token]);
-  
+
   const OnKeyPress = (e: any) => {
-    if (keyword === "") {
-      alert("검색어를 입력해주세요");
-    } else if (e.key === "Enter") {
+    if (keyword === '') {
+      alert('검색어를 입력해주세요');
+    } else if (e.key === 'Enter') {
       onSubmit();
     }
   };
@@ -91,15 +91,15 @@ function Header() {
     );
     setFilteredItems(filtered);
   };
-  
+
   useEffect(() => {
     filterItems(keyword);
-  }, [keyword, product]); 
+  }, [keyword, product]);
 
   async function getItem() {
     const cate = {
-      limit : 100
-    }
+      limit: 100,
+    };
     const data = await getList(cate);
     console.log(data);
     setProductInfo(data);
@@ -116,7 +116,7 @@ function Header() {
       <header className="headerContainer">
         <div className="itemsWrapper">
           <Link to="/" className="logoBox">
-            <img src="/public/images/Wink_logo.png" alt="logo" />
+            <img src="/images/Wink_logo.png" alt="logo" />
           </Link>
           <div className="searchBox">
             <input
@@ -126,44 +126,41 @@ function Header() {
               onKeyPress={OnKeyPress}
             />
             <img
-              src="/public/images/search-icon.png"
+              src="/images/search-icon.png"
               alt="searchicon"
               onClick={handleInputButtonClick}
             />
 
-            {showInputButton && keyword &&(
-          <div className="Input-Buttom">
-            <div className="Input-Buttom__inner">
-              {keyword &&
-                filteredItems.map((v: Product) => {
-                  if (v.product_name.trim() !== "") {
-                    return (
-                    <Link
-                      to={`/detail/${v.product_no}`}
-                      key={v.product_no}
-                      className="Input-Buttom__innerBox"
-                      >
-                      <div className="Input-Buttom__ImageBox">
-                        <img src={v.small_image} alt="searchbookimage" />
-                      </div>
+            {showInputButton && keyword && (
+              <div className="Input-Buttom">
+                <div className="Input-Buttom__inner">
+                  {keyword &&
+                    filteredItems.map((v: Product) => {
+                      if (v.product_name.trim() !== '') {
+                        return (
+                          <Link
+                            to={`/detail/${v.product_no}`}
+                            key={v.product_no}
+                            className="Input-Buttom__innerBox"
+                          >
+                            <div className="Input-Buttom__ImageBox">
+                              <img src={v.small_image} alt="searchbookimage" />
+                            </div>
 
-                      <div className="Input-Buttom__title">
-                        <span>{v.product_name}</span>
-                        <span>{v.price.slice(0, -3)}원</span>
-                      </div>  
-                    </Link>   
-                    );
-                  } else {
-                    return null;
-                  }
-                })}
-            </div>
+                            <div className="Input-Buttom__title">
+                              <span>{v.product_name}</span>
+                              <span>{v.price.slice(0, -3)}원</span>
+                            </div>
+                          </Link>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-
-          
-
 
           <div className="Header-box">
             <Link className="Header-box__text" to="/cart">
@@ -178,11 +175,9 @@ function Header() {
                   로그아웃
                 </div>
                 <div className="cart">
-                  <img 
-                    className="cartPhoto"
-                    src={user.profileImg} />
-                  </div>
-              </div> 
+                  <img className="cartPhoto" src={user.profileImg} />
+                </div>
+              </div>
             ) : (
               <>
                 <Link className="Header-box__text" to="/join">
@@ -195,11 +190,7 @@ function Header() {
             )}
           </div>
         </div>
-
-       
       </header>
-
-    
     </>
   );
 }
